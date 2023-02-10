9285ad62a3a8fa13493903605e71aadfc6c7661c
gokrusk <nigelljama@gmail.com>
2023-02-10 11:57:12 -0500

    query

--- database/queries.js
+++ database/queries.js
@@ -4,7 +4,7 @@ const tableName = 'PRODUCTO'
 export const queries = {
     getAllProducts: `SELECT * FROM ${tableName}`,
     getProduct: `SELECT * FROM ${tableName} WHERE cat_id = @Id`,
-    countProducts: `SELECT COUNT(cat_id) FROM PRODUCTO`,
+    countProducts: `SELECT COUNT(cat_id) FROM ${tableName}`,
     createProduct: `INSERT INTO ${tableName} (cat_nom, cat_os) VALUES (@nombre, @descripcion)`,
     deleteProduct: `DELETE FROM ${tableName} WHERE cat_id = @Id`,
     updateProduct: `UPDATE ${tableName} SET cat_nom = @cat_nom, cat_os = @cat_os WHERE cat_id = @cat_id`
